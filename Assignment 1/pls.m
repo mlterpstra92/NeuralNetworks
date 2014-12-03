f6101cabc41835ef6cb12e4bed2619b0cc1a9fb6
hankruiger <hankruiger@gmail.com>
2014-12-03 14:43:50 +0100

    New version of binomial stuff

--- Assignment 1/pls.m
+++ Assignment 1/pls.m
@@ -5,9 +5,8 @@ if P <= N
     prob = 1;
 else
     prob = 0;
-    factpminus1 = factorial(P - 1);
     for i=0:N-1
-        prob = prob + factpminus1 / (factorial(i) * factorial(P - 1 - i));
+        prob = prob + nck(P-1, i);
     end
     prob = 2^(1-P) * prob;
 end
