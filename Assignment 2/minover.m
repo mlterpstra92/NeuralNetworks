de2e196ebe720c5ff41c46979ebecff275de7c49
Maarten Terpstra <m.l.terpstra@student.rug.nl>
2015-01-06 22:11:31 +0100

    Added comment and initial workload division

--- Assignment 2/minover.m
+++ Assignment 2/minover.m
@@ -7,18 +7,27 @@ function [w, errors] = minover( delta, n_max, P, N, xi, S, teacherVector)
     t = 1;
     changes = ones(1, P);
     while true
+        % Determine which element is the least stable
         [~, mu] = determineMinimalOverlap(w, xi, S);
+        
+        % Update the student weight vector
         w_new = w + ((xi(:, mu) * S(mu)) / N);
+        % Store the generalized error
         errors = [errors determineError(w_new, teacherVector)];
+        % Store the angle between the old and new vector as measure for
+        % change
         change = (1 - dot(w, w_new) / (norm(w)*norm(w_new)));
         changes(mod(t, P) + 1) = change;
         t = t + 1;
+        % If there are P epochs without significant change, exit
         if (all(changes < delta))
             break
+        % If too many epochs have passed, exit
         elseif t >= n_max * P
             disp('Your time has passed');
             break
         end
+        % Stabilize further with new weight vector
         w = w_new;
     end
 end
