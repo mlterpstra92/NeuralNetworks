50b92b861f7a78baabe059a7d5a27dae03908999
Maarten Terpstra <m.l.terpstra@student.rug.nl>
2015-01-06 16:20:55 +0100

    more report stuff and more code

--- Assignment 2/alpha_plot.m
+++ Assignment 2/alpha_plot.m
@@ -5,12 +5,16 @@ delta = 1e-5;
 n_max = 400;
 teacherVector = ones(N, 1);
 learning_curve = zeros(size(alphas));
-
+innerlimit = 10;
 parfor i=1:length(alphas)
-    alpha = alphas(i);
-    P = round(alpha * N);
+    alpha = alphas(i);        
     disp(['Calculating minover for alpha = ', num2str(alpha)]);
-    learning_curve(i) = minover_generalization_error(N, P, delta, n_max, teacherVector, 0);
+    innervals = zeros(1, innerlimit);
+    for j=1:innerlimit
+        P = round(alpha * N);
+        innervals(j) = minover_generalization_error(N, P, delta, n_max, teacherVector, 0);
+    end
+    learning_curve(i) = mean(innervals);
 end
 
 plot(alphas, learning_curve);
--- Assignment 2/runMinover.m
+++ Assignment 2/runMinover.m
@@ -1,8 +1,8 @@
 N = 20;
 P = 3*N;
-delta = 10^-4;
+delta = 10^-5;
 teacherVector = ones(N, 1);
-[xi, S] = generateDatasets(P, N, teacherVector, .1);
+[xi, S] = generateDatasets(P, N, teacherVector);
 [w_det, errors] = minover(delta, 200, P, N, xi, S, teacherVector);
 diff = sign(xi' * w_det) == S';
 numDiffs = length(find(diff == 0));
@@ -10,4 +10,8 @@ disp([num2str(numDiffs), ' differences']);
 if numDiffs > 0
     disp(num2str(diff));
 end
-plot(1:length(errors), errors)
+plot(1:length(errors), errors);
+
+xlabel('time');
+ylabel('Generalization error');
+title('Generalization error as a funtion of the time');
