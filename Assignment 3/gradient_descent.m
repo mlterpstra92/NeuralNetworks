93bf12b84a501556246ec1f28b4f51aae2f83ee8
hankruiger <hankruiger@gmail.com>
2015-01-10 17:09:34 +0100

    a very little bit more work

--- Assignment 3/gradient_descent.m
+++ Assignment 3/gradient_descent.m
@@ -1,6 +1,11 @@
+N = 50;
+P = 5000;
+
 % Generate the dataset
 input_limits = [-3, 3];
 output_limits = [-1, 1];
-[xis, taus] = generate_dataset(50, 5000, input_limits, output_limits);
+[xis, taus] = generate_dataset(N, P, input_limits, output_limits);
 
 % Initialize the weight vectors
+w_1 = generate_weight_vector(N);
+w_2 = generate_weight_vector(N);
